974a80189bbce78d36e7509864154856db96a074
Jpgomera19 <jpgomera19@gmail.com>
2023-02-05 22:54:30 +0800

    ALEGARIO

---
+++ Hospital-template/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'acme/application-tracker',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dd6ec8a2a10449427dd4de763e329466a0b34aae',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'acme/application-tracker' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dd6ec8a2a10449427dd4de763e329466a0b34aae',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
