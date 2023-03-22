0f3365aab4b9bd64fdc441e58627eb232af3eea2
Jpgomera19 <jpgomera19@gmail.com>
2023-03-22 19:35:51 +0800

    Updated

--- database/connection.php
+++ database/connection.php
@@ -9,3 +9,14 @@ if(!$con){
     echo "Failed to connect";
     die();
 }
+
+// $localhost = "localhost";
+// $user = "u685566035_hospital";
+// $pass = "Hospital123456789";
+// $database = "u685566035_hospital";
+
+// $con = mysqli_connect($localhost, $user, $pass, $database);
+// if(!$con){
+//     echo "Failed to connect";
+//     die();
+// }
